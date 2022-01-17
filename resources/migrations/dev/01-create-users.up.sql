5535b05befde95cc4c5ed59bd533e0c038bc2e22
Mitsuru GIMA <gima0326@yahoo.co.jp>
2022-01-17 23:46:16 +0900

    Update 01-create-users.up.sql

--- resources/migrations/dev/01-create-users.up.sql
+++ resources/migrations/dev/01-create-users.up.sql
@@ -1,1 +1,5 @@
 CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT, name text, age int, email text, PRIMARY KEY(id));
+--;;
+INSERT INTO users (name, age, email) values ('test_user', 20, 'test_user@yahoo.co.jp');
+--;;
+INSERT INTO users (name, age, email) values ('test_user2', 21, 'test_user2@yahoo.co.jp');

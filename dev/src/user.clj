8fe54db6604b47ae8d9b4c6c78cd3c61bb53d06b
Mitsuru GIMA <gima0326@yahoo.co.jp>
2022-01-18 10:06:30 +0900

    Update user.clj

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -6,3 +6,18 @@
   (require 'dev)
   (in-ns 'dev)
   :loaded)
+
+;; add
+(def migration-db-urls
+  {"dev"  "jdbc:mysql://localhost:3306/dev?user=root&password=password"
+   "test" "jdbc:mysql://localhost:3306/test?user=root&password=password"})
+
+(defn- migration-config [env]
+  {:datastore  (jdbc/sql-database {:connection-uri (migration-db-urls env)})
+   :migrations (jdbc/load-resources "migrations")})
+
+(defn migrate [env]
+  (repl/migrate (migration-config env)))
+
+(defn rollback [env]
+  (repl/rollback (migration-config env)))
